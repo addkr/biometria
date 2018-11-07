32a9e80c00981c77724468caa8c3b28d137b7929
unknown <adr.kr@wp.pl>
2018-11-07 20:14:09 +0100

    Add existing file

---
+++ Biometria/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Biometria
+{
+    partial class FormBiometria
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowNumbers = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.range1 = new System.Windows.Forms.RadioButton();
+            this.range2 = new System.Windows.Forms.RadioButton();
+            this.range3 = new System.Windows.Forms.RadioButton();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnShowNumbers
+            // 
+            this.btnShowNumbers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnShowNumbers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShowNumbers.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnShowNumbers.ForeColor = System.Drawing.Color.White;
+            this.btnShowNumbers.Location = new System.Drawing.Point(130, 174);
+            this.btnShowNumbers.Name = "btnShowNumbers";
+            this.btnShowNumbers.Size = new System.Drawing.Size(158, 34);
+            this.btnShowNumbers.TabIndex = 0;
+            this.btnShowNumbers.Text = "LOSUJ HASŁO";
+            this.btnShowNumbers.UseVisualStyleBackColor = false;
+            this.btnShowNumbers.Click += new System.EventHandler(this.randomNumbers);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.range3);
+            this.groupBox.Controls.Add(this.range2);
+            this.groupBox.Controls.Add(this.range1);
+            this.groupBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.groupBox.Location = new System.Drawing.Point(112, 46);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(200, 100);
+            this.groupBox.TabIndex = 1;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "WYBIERZ ZAKRES";
+            // 
+            // range1
+            // 
+            this.range1.AutoSize = true;
+            this.range1.Location = new System.Drawing.Point(26, 21);
+            this.range1.Name = "range1";
+            this.range1.Size = new System.Drawing.Size(88, 20);
+            this.range1.TabIndex = 0;
+            this.range1.TabStop = true;
+            this.range1.Text = "ZAKRES 1";
+            this.range1.UseVisualStyleBackColor = true;
+            // 
+            // range2
+            // 
+            this.range2.AutoSize = true;
+            this.range2.Location = new System.Drawing.Point(26, 44);
+            this.range2.Name = "range2";
+            this.range2.Size = new System.Drawing.Size(88, 20);
+            this.range2.TabIndex = 1;
+            this.range2.TabStop = true;
+            this.range2.Text = "ZAKRES 2";
+            this.range2.UseVisualStyleBackColor = true;
+            // 
+            // range3
+            // 
+            this.range3.AutoSize = true;
+            this.range3.Location = new System.Drawing.Point(26, 68);
+            this.range3.Name = "range3";
+            this.range3.Size = new System.Drawing.Size(88, 20);
+            this.range3.TabIndex = 2;
+            this.range3.TabStop = true;
+            this.range3.Text = "ZAKRES 3";
+            this.range3.UseVisualStyleBackColor = true;
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.White;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox.Location = new System.Drawing.Point(178, 251);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(68, 31);
+            this.textBox.TabIndex = 2;
+            this.textBox.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(437, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(44, 44);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.startRecording);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.Location = new System.Drawing.Point(184, 44);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(80, 40);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.stopRecording);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(91, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NAGRYWANIE";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(437, 190);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(293, 117);
+            this.panel2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(114, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "WYNIK";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.richTextBox1.Location = new System.Drawing.Point(44, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(200, 57);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // FormBiometria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.btnShowNumbers);
+            this.Name = "FormBiometria";
+            this.Text = "System Rozpoznawania Mówcy";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowNumbers;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.RadioButton range3;
+        private System.Windows.Forms.RadioButton range2;
+        private System.Windows.Forms.RadioButton range1;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Biometria/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biometria
+{
+    public partial class FormBiometria : Form
+    {
+
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder retstring, int returnLength, IntPtr callback);
+
+        public FormBiometria()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void randomNumbers(object sender, EventArgs e)
+        {
+            string randomSequence = "";
+            Random rnd = new Random();
+            for(int i=0; i < 5; i++)
+            {
+                int number = rnd.Next(0, 9);
+                randomSequence += number;
+            }
+            
+            textBox.Text = randomSequence;
+        }
+
+        private void startRecording(object sender, EventArgs e)
+        {
+            mciSendString("Open new type waveaudio alias recsound", null, 0, IntPtr.Zero);
+            mciSendString("Record recsound", null, 0, IntPtr.Zero);
+        }
+
+        private void stopRecording(object sender, EventArgs e)
+        {
+            string date = DateTime.Now.ToString();
+            date = date.Replace(":", "");
+            date = date.Replace("/", "");
+            date = date.Replace(" ", "");
+            date = date.Replace(".", "");
+            string path = "Save recsound c:\\biometria\\"+date+".wav";
+            //Console.Out.Write(path);
+            //"Save recsound c:\\biometria\\sound.wav"
+            mciSendString(path, null, 0, IntPtr.Zero);
+            mciSendString("Close recsound", null, 0, IntPtr.Zero);
+        }
+
+    }
+}
