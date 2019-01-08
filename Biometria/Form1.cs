1a352a406b5bbcb201099fea8fec2eaec1897dfd
Katarzyna <sinkiewicz.kasia@gmail.com>
2019-01-08 23:28:15 +0100

    wczytywanie ramek

--- Biometria/Form1.cs
+++ Biometria/Form1.cs
@@ -1,15 +1,10 @@
 ﻿using NAudio.Wave;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Biometria
@@ -17,17 +12,18 @@ namespace Biometria
     public partial class FormBiometria : Form
     {
         string range = "";
-        double[] sourceFFT;
-        double[] sourceFFT1;
-        double[] newFFT;
+        string access = "";
+        List<double> sourceFFT;
+        List<double> newFFT;
         WaveIn sourceStream;
         WaveFileWriter waveWriter;
         readonly String FilePath;
         public WaveIn wi;
         public BufferedWaveProvider bwp;
         public Int32 envelopeMax;
-        private int RATE = 44100; 
+        private int RATE = 44100;
         private int BUFFERSIZE = (int)Math.Pow(2, 11);
+
 
         [DllImport("winmm.dll")]
         private static extern long mciSendString(string command, StringBuilder retstring, int returnLength, IntPtr callback);
@@ -36,17 +32,19 @@ namespace Biometria
         {
             InitializeComponent();
             FilePath = "..\\..\\Sounds\\";
+            sourceFFT = new List<double>();
+
+            newFFT = new List<double>();
         }
 
-      
+
         public void startRecording(object sender, EventArgs e)
         {
-            newFFT = null;
             sourceStream = new WaveIn
             {
 
                 WaveFormat =
-                    new WaveFormat(RATE, 2)
+                    new WaveFormat(RATE, 1)
             };
 
             sourceStream.DataAvailable += this.SourceStreamDataAvailable;
@@ -54,7 +52,7 @@ namespace Biometria
             if (!Directory.Exists(FilePath))
             {
                 Directory.CreateDirectory(FilePath);
-            }            
+            }
             waveWriter = new WaveFileWriter(@"..\\..\\Sounds\\check.wav", sourceStream.WaveFormat);
             sourceStream.StartRecording();
         }
@@ -80,103 +78,77 @@ namespace Biometria
             }
             this.waveWriter.Dispose();
             this.waveWriter = null;
-            readWavStream("..\\..\\Sounds\\check.wav", false, "");
+            readWavStream("..\\..\\Sounds\\check.wav", false);
+            //readWavStream("..\\..\\Sounds\\Kasia\\" + range + "\\" + 3 + "\\" + textBox.Text + ".wav", false);
         }
 
         private void randomNumbers(object sender, EventArgs e)
         {
-            Random rnd = new Random();           
+            Random rnd = new Random();
             int number = rnd.Next(0, 9);
             textBox.Text = number.ToString();
         }
 
-    
-        public void readWavStream(string filename, bool isCompraing, string access)
+
+        public void readWavStream(string filename, bool isCompraing)
         {
-            try
+
+            if (isCompraing)
             {
-                using (FileStream fs = File.Open(filename, FileMode.Open))
+                sourceFFT = new List<double>();
+            }
+            else
+            {
+                newFFT = new List<double>();
+            }
+
+
+            using (FileStream fs = File.Open(filename, FileMode.Open))
+            {
+                BinaryReader reader = new BinaryReader(fs);
+                int frameSize = BUFFERSIZE;
+
+                var cursor = 0;
+                while (true)
                 {
-                    BinaryReader reader = new BinaryReader(fs);
 
-                    int bytes = reader.ReadInt32();
-                    byte[] byteArray = reader.ReadBytes(bytes);
-                    //newFFT = new double[byteArray.Length];
-                    int count = byteArray.Length / BUFFERSIZE;
-                    for (int j = 0; j < count; j++)
+
+                    var frames = new byte[frameSize];
+                    var bytesRead = reader.Read(frames, 0, frameSize);
+                    Console.WriteLine(bytesRead);
+                    if (bytesRead < frameSize)
                     {
-                        int frameSize = BUFFERSIZE;
-                        var frames = new byte[frameSize];
-                        Array.Copy(byteArray, j * frameSize, frames, 0, frameSize);
-                        //reader.Read(frames, j*frameSize, j*frameSize + frameSize);
-                        //for(int k = 0; k < frameSize; k++)
-                        //{
-                        //    frames[k] = byteArray[j * frameSize + k];
-                        //}
-                        if (frames.Length == 0) return;
-
-                        int SAMPLE_RESOLUTION = 16;
-                        int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
-                        Int32[] vals = new Int32[frames.Length / BYTES_PER_POINT];
-                        double[] Ys = new double[frames.Length / BYTES_PER_POINT];
-                        for (int i = 0; i < vals.Length; i++)
-                        {
-                            byte hByte = frames[i * 2 + 1];
-                            byte lByte = frames[i * 2 + 0];
-                            vals[i] = (int)(short)((hByte << 8) | lByte);
-                            Ys[i] = vals[i];
-                        }
-                        if (isCompraing == true)
-                        {
-                            
-                            if (access == "Ada")
-                            {
-                                if (sourceFFT == null)
-                                {
-                                    sourceFFT = FFT(Ys);
-                                }
-                                else
-                                {
-                                    sourceFFT.Concat(FFT(Ys));
-                                }
-
-                            }
-                            else if(access == "Kasia")
-                            {
-                                if (sourceFFT1 == null)
-                                {
-                                    sourceFFT1 = FFT(Ys);
-                                }
-                                else
-                                {
-                                    sourceFFT1.Concat(FFT(Ys));
-                                }
-
-                            }                           
-                            
-                        }
-                        else if (isCompraing == false)
-                        {
-
-                            if (newFFT == null)
-                            {
-                                newFFT = FFT(Ys);
-                            }
-                            else
-                            {
-                                newFFT.Concat(FFT(Ys));
-                            }
-                        }
+                        break;
+                        //return;
                     }
+                    
 
+                    int SAMPLE_RESOLUTION = 16;
+                    int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
+                    Int32[] vals = new Int32[frames.Length / BYTES_PER_POINT];
+                    double[] Ys = new double[frames.Length / BYTES_PER_POINT];
+
+                    for (int i = 0; i < vals.Length; i++)
+                    {
+                        byte hByte = frames[i * 2 + 1];
+                        byte lByte = frames[i * 2 + 0];
+                        vals[i] = (int)((hByte << 8) | lByte);
+                        Ys[i] = vals[i];
+                    }
+                    if (isCompraing == true)
+                    {
+                        sourceFFT.AddRange(FFT(Ys));
+                    }
+                    else if (isCompraing == false)
+                    {
+                        newFFT.AddRange(FFT(Ys));
+                    }
+                    cursor += BUFFERSIZE;
                 }
             }
-            catch(Exception ex)
-            {
-                Console.Out.WriteLine(ex.Message);
-            }
+
         }
-        
+
         public double[] FFT(double[] data)
         {
             double[] fft = new double[data.Length];
@@ -187,7 +159,8 @@ namespace Biometria
             for (int i = 0; i < data.Length; i++)
             {
                 fft[i] = fftComplex[i].Magnitude;
-            }                
+                //Console.Out.WriteLine(fft[i]);
+            }
             return fft;
         }
 
@@ -199,32 +172,29 @@ namespace Biometria
         private void range2_CheckedChanged(object sender, EventArgs e)
         {
             range = "48000Hz";
-        }      
+        }
 
-    
         private void button1_Click(object sender, EventArgs e)
         {
             List<double> resultsList = new List<double>();
             List<double> resultsList1 = new List<double>();
             for (int i = 1; i < 4; i++)
             {
-                sourceFFT = null;
                 string fileName = "..\\..\\Sounds\\Ada\\" + range + "\\" + i + "\\" + textBox.Text + ".wav";
-                readWavStream(fileName, true, "Ada");
+                readWavStream(fileName, true);
                 resultsList.Add(ComputeCoeff(sourceFFT, newFFT));
             }
-            for (int j = 1; j < 4; j++)
+            for (int i = 1; i < 4; i++)
             {
-                sourceFFT1 = null;
-                string fileName = "..\\..\\Sounds\\Kasia\\" + range + "\\" + j + "\\" + textBox.Text + ".wav";
-                readWavStream(fileName, true, "Kasia");
-                resultsList1.Add(ComputeCoeff(sourceFFT1, newFFT));
+                string fileName = "..\\..\\Sounds\\Kasia\\" + range + "\\" + i + "\\" + textBox.Text + ".wav";
+                readWavStream(fileName, true);
+                resultsList1.Add(ComputeCoeff(sourceFFT, newFFT));
             }
             checkResult(resultsList, resultsList1);
         }
 
-       
-        public double ComputeCoeff(double[] values1, double[] values2)
+
+        public double ComputeCoeff(List<double> values1, List<double> values2)
         {
             var avg1 = values1.Average();
             var avg2 = values2.Average();
@@ -234,17 +204,17 @@ namespace Biometria
             var sumSqr1 = values1.Sum(x => Math.Pow((x - avg1), 2.0));
             var sumSqr2 = values2.Sum(y => Math.Pow((y - avg2), 2.0));
 
-            var result = sum1 / Math.Sqrt(sumSqr1 * sumSqr2);            
+            var result = sum1 / Math.Sqrt(sumSqr1 * sumSqr2);
             return result;
         }
 
         public void checkResult(List<double> list, List<double> list1)
         {
-           
+
             int a = 0;
             double sum = 0;
             double sum1 = 0;
-            for(int i=0; i < list.Count; i++)
+            for (int i = 0; i < list.Count; i++)
             {
                 sum += list[i];
                 sum1 += list1[i];
@@ -252,11 +222,11 @@ namespace Biometria
             sum = sum / 3;
             sum1 = sum1 / 3;
             string result = "Ada: " + sum + " Kasia: " + sum1;
-            
+
             richTextBox1.Text = result;
         }
 
-       
+
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
